a3b29514bb3e52b1d2c491e5266f938df1774c31
tanmay3107 <tanmay.janak.tj1113@gmail.com>
2026-01-28 08:28:15 +0530

    langchain retrivalQA depricated

--- ask_rag.py
+++ ask_rag.py
@@ -1,7 +1,7 @@
 from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.chat_models import ChatOpenAI
-from langchain_community.chains import RetrievalQA
+from langchain_community.chains.retrieval_qa.base import RetrievalQA
 
 # 1. Setup Database
 print("🧠 Loading Memory...")
